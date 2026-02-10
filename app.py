95677db550f444062011319673292aba3d7fa162
Sirisha Pamidipati <104159397+SirishaP2903@users.noreply.github.com>
2026-02-10 05:55:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,3 +40,10 @@ def predict():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+from flask import Flask, render_template
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
